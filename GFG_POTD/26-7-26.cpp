1a5b8271fab19b219454840270b5efb28331b15c
vikasmourya-mnnit <vikas443407@gmail.com>
2026-07-26 22:25:40 +0530

    sort the level of binary tree

---
+++ GFG_POTD/26-7-26.cpp
@@ -0,0 +1,31 @@
+class Solution {
+	public:
+	vector<vector<int>> levelSort(vector<int>& arr) {
+		// code here
+		queue<pair<int, int>> pq;
+		pq.push({arr[0], 0}); // val , idx
+		int n = arr.size();
+		vector<vector<int>>ans;
+		while (!pq.empty()) {
+			int sz = pq.size();
+			vector<int>temp;
+			while (sz--) {
+				int x = pq.front().first;
+				int idx = pq.front().second;
+				pq.pop();
+				
+				temp.push_back(x);
+				if (2*idx + 1<n) {
+					pq.push({arr[2*idx + 1], 2*idx + 1});
+				}
+				if (2*idx + 2<n) {
+					pq.push({arr[2*idx + 2], 2*idx + 2});
+				}
+				
+			}
+			sort(temp.begin(), temp.end());
+			ans.push_back(temp);
+		}
+		return ans;
+	}
+};
